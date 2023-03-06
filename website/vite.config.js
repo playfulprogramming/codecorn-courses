491948a5031fc2fdc47805f629d2de8348332632
Stoney Eagle <patrickstaarink@hotmail.com>
2023-03-06 17:06:45 +0100

    Feat Laravel auth, Vite server (rebuild docker)

--- website/vite.config.js
+++ website/vite.config.js
@@ -4,8 +4,23 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
+    server: {
+        port: 8766,
+        hmr: {
+            host: 'localhost',
+        },
+        watch: {
+            usePolling: true,
+        }
+    },
+    hmr: {
+        host: '0.0.0.0'
+    }
 });
